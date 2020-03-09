78964755c47f4106e0bcfa01b1127be65906f0cd
Jennifer Ingram <jennifer.ingram@flatironschool.com>
2020-03-09 14:26:06 -0400

    completed manual filter method

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, string) {
+    let matchingDrivers = []
+    drivers.forEach( driver => {
+        if (driver.toLowerCase() === string.toLowerCase()) {
+            matchingDrivers.push(driver)
+        }
+    })
+    return matchingDrivers
+}
+
+function fuzzyMatch(drivers, substring) {
+    let matchingDrivers = []
+    drivers.forEach( driver => {
+        if (driver.toLowerCase().startsWith(substring.toLowerCase())) {
+            matchingDrivers.push(driver)
+        }
+    })
+    return matchingDrivers
+}
+
+function matchName(drivers, name) {
+    let matchingDrivers = []
+    drivers.forEach( driver => {
+        if (driver["name"].toLowerCase() === name.toLowerCase()) {
+            matchingDrivers.push(driver)
+        }
+    })
+    return matchingDrivers
+}
